317b2f6e021ef26ce9b0f9be0e872fc38ad1d86c
Vishwaraj <vishwarajk96@gmail.com>
2022-07-07 16:13:06 +0530

    added PORT

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const app = express()
 
+const PORT = 4000;
+
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
-app.listen(3000);
+app.listen(PORT, () => console.log(`App started at ${PORT}`));
+
+// control + c stops the server //
