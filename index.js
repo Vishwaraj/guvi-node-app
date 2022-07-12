f4defda0ca260e28853ddb38ebcf1317ce573754
Vishwaraj <vishwarajk96@gmail.com>
2022-07-13 01:43:20 +0530

    Added routes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express, { application } from "express";
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 import { ObjectId } from "mongodb"; // this is needed to fetch data using object id as we have to convert it to
+import {moviesRouter} from './routes/movies.js'
 
 dotenv.config();
 
@@ -37,78 +38,10 @@ async function createConnection() {
 }
 
 //mongo db connection function call --
-const client = await createConnection();
+export const client = await createConnection();
 
 //----------MONGODB CONNECTION CODE ENDS-------------------------------
 
-//------- get movies route and search functionality--------------------
-app.get("/movies", async function (request, response) {
-  console.log(request.query);
-  if (request.query.rating) {
-    request.query.rating = +request.query.rating;
-  }
 
-  const allMovies = await client
-    .db("guvi-db")
-    .collection("movies")
-    .find(request.query)
-    .toArray();
-  response.send(allMovies);
-});
-
-//--------THIS REQUEST IS GETTING MOVIE DATA FROM MONGODB----------------
-app.get("/movies/:id", async function (request, response) {
-  const { id } = request.params;
-  console.log(request.params);
-  // const movie = movieData.find((movie) => movie.id === id);
-
-  //this code fetches single movie data from mongoDB
-  const movie = await client
-    .db("guvi-db")
-    .collection("movies")
-    .findOne({ id: id });
-
-  movie ? response.send(movie) : response.status(404).send("Not Found");
-});
-
-//POST REQUEST TO MONGODB
-
-app.post("/movies", express.json(), async function (request, response) {
-  const data = request.body;
-
-  const status = await client
-    .db("guvi-db")
-    .collection("movies")
-    .insertMany(data);
-
-  response.send(status);
-});
-
-//DELETE REQUEST TO MONGO DB
-
-app.delete("/movies/:id", async function (request, response) {
-  const { id } = request.params;
-
-  const deleted = await client
-    .db("guvi-db")
-    .collection("movies")
-    .deleteOne({ id: id });
-
-  deleted.result > 0
-    ? response.send(deleted)
-    : response.send({ msg: "no movie found" });
-});
-
-//UPDATE MOVIE REQUEST TO MONGODB
-
-app.put("/movies/:id", async function (request, response) {
-  const { id } = request.params;
-  const data = request.body;
-
-  const updated = await client
-    .db("guvi-db")
-    .collection("movies")
-    .updateOne({ id: id }, { $set: data });
-
-  response.send(updated);
-});
+// for movie routes
+app.use("/movies", moviesRouter);
---
+++ routes/movies.js
@@ -0,0 +1,81 @@
+import express from "express";
+const router = express.Router();
+import {client} from '../index.js'
+
+// All Movie Routes
+
+
+//------- get movies route and search functionality--------------------
+router.get("/", async function (request, response) {
+    console.log(request.query);
+    if (request.query.rating) {
+      request.query.rating = +request.query.rating;
+    }
+  
+    const allMovies = await client
+      .db("guvi-db")
+      .collection("movies")
+      .find(request.query)
+      .toArray();
+    response.send(allMovies);
+  });
+  
+  //--------THIS REQUEST IS GETTING MOVIE DATA FROM MONGODB----------------
+  router.get("/:id", async function (request, response) {
+    const { id } = request.params;
+    console.log(request.params);
+    // const movie = movieData.find((movie) => movie.id === id);
+  
+    //this code fetches single movie data from mongoDB
+    const movie = await client
+      .db("guvi-db")
+      .collection("movies")
+      .findOne({ id: id });
+  
+    movie ? response.send(movie) : response.status(404).send("Not Found");
+  });
+  
+  //POST REQUEST TO MONGODB
+  
+  router.post("/", express.json(), async function (request, response) {
+    const data = request.body;
+  
+    const status = await client
+      .db("guvi-db")
+      .collection("movies")
+      .insertMany(data);
+  
+    response.send(status);
+  });
+  
+  //DELETE REQUEST TO MONGO DB
+  
+  router.delete("/:id", async function (request, response) {
+    const { id } = request.params;
+  
+    const deleted = await client
+      .db("guvi-db")
+      .collection("movies")
+      .deleteOne({ id: id });
+  
+    deleted.result > 0
+      ? response.send(deleted)
+      : response.send({ msg: "no movie found" });
+  });
+  
+  //UPDATE MOVIE REQUEST TO MONGODB
+  
+  router.put("/:id", async function (request, response) {
+    const { id } = request.params;
+    const data = request.body;
+  
+    const updated = await client
+      .db("guvi-db")
+      .collection("movies")
+      .updateOne({ id: id }, { $set: data });
+  
+    response.send(updated);
+  });
+  
+
+  export const moviesRouter = router;
